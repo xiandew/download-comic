656bcb6ddc1e00abea7e03cea67464573ad6fc85
Xiande Wen <visiande1998@gmail.com>
2019-05-10 16:20:07 +1000

    Add readme

--- index.php
+++ index.php
@@ -2,20 +2,6 @@
 /**
  * Created by Xiande Wen, November 2017
  * Last modified: 10:25, 2019-05-09
- *
- * This is a program for downloading pictures and extracting information
- * about a comic from <http://www.acg456.com/> for personal interests.
- * Regular expresssions are used to mathch Particular section of a page.
- * I understand that the design of this program is not so good since it
- * hardwires strings to some extent but it serves the functionality.
- *
- * This is an updated version. The original version is not suitable for
- * the purpose due to the reformation of the website since the execution
- * of this program requires exact match of particular contents.
- *
- * To run this program, be sure that the server and PHP are configured
- * properly and then simply try to reach this file from any browser through
- * localhost (eg. http://localhost/download-comics/index.php).
  */
 include_once("file.php");
 include_once("download.php");
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Download comics
+
+This is a program for downloading pictures and extracting information
+about a comic from http://www.acg456.com/ for personal interests.
+Regular expresssions are used to mathch Particular section of a page.
+I understand that the design of this program is not so good since it
+hardwires strings to some extent but it serves the functionality.
+
+This is an updated version. The original version is not suitable for
+the purpose due to the reformation of the website since the execution
+of this program requires exact match of particular contents.
+
+To run this program, be sure that the server and PHP are configured
+properly and then simply try to reach this file from any browser through
+localhost (eg. `http://localhost/download-comics/index.php`).
